9bdd18155eb76f83ea147823b61ce9b69d3a8829
Jackson Peterson <jacksonlpeterson@gmail.com>
2022-02-16 17:47:14 -0700

    Final

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-
-
-
 document.getElementById("weatherSubmit").addEventListener("click", function(event) {
-    event.preventDefault();
-    const value = document.getElementById("weatherInput").value;
-    if (value === "")
-      return;
-    console.log(value);
-  });
-
+  event.preventDefault();
+  const value = document.getElementById("weatherInput").value;
+  if (value === "")
+    return;
+  console.log(value);
   const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=76ba025adce76719aef920ee33eb2297";
   fetch(url)
     .then(function(response) {
       return response.json();
     }).then(function(json) {
-      console.log(json);
-      
-    });
-
-    let results = "";
+      let results = "";
       results += '<h2>Weather in ' + json.name + "</h2>";
       for (let i=0; i < json.weather.length; i++) {
 	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
@@ -32,3 +23,28 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
       }
       results += "</p>";
       document.getElementById("weatherResults").innerHTML = results;
+    });
+});
+
+
+
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("weatherInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=76ba025adce76719aef920ee33eb2297";
+fetch(url2)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let forecast = "";
+    for (let i=0; i < json.list.length; i++) {
+forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
+forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+    }
+    document.getElementById("forecastResults").innerHTML = forecast;
+  });
+});
