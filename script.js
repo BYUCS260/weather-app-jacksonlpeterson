d441a87571fdf402494d0b3b04bb918b532aa8a5
Jackson Peterson <jacksonlpeterson@gmail.com>
2022-02-23 14:27:40 -0700

    Fixed Layout

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
       return response.json();
     }).then(function(json) {
       let results = "";
-      results += '<h2>Weather in ' + json.name + "</h2>";
+      results += '<h2 class="place" >Weather in ' + json.name + "</h2>";
       for (let i=0; i < json.weather.length; i++) {
-	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+	results += '<img class="icon" src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
       }
-      results += '<h2>' + json.main.temp + " &deg;F</h2>"
-      results += "<p>"
+      results += '<h2 class="heading">' + json.main.temp + " &deg;F</h2>"
+      results += '<p class="report">'
       for (let i=0; i < json.weather.length; i++) {
 	results += json.weather[i].description
 	if (i !== json.weather.length - 1)
@@ -41,9 +41,9 @@ fetch(url2)
   }).then(function(json) {
     let forecast = "";
     for (let i=0; i < json.list.length; i++) {
-forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+forecast += '<h2 class="heading">' + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
+forecast += '<p class="report">Temperature: ' + json.list[i].main.temp + "</p>";
+forecast += '<img class ="icon" src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
     }
     document.getElementById("forecastResults").innerHTML = forecast;
   });
